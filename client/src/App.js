dfd7d006eb2129d762180270feb000ec0bff23e3
kpegeder <k.pegeder@gmail.com>
2021-01-17 16:56:15 -0800

    add navbar and start a menu page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Menu from "./pages/Menu";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/menu">
+          <Menu />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
